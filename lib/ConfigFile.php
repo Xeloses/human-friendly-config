5ff4ccf218de7e32b139d87b52e3ca5e7e76923e
Xeloses <xeloses@gmail.com>
2020-07-13 14:52:25 +0300

    v1.0.1

--- lib/ConfigFile.php
+++ lib/ConfigFile.php
@@ -5,7 +5,7 @@
  *
  * @author     Xeloses (https://github.com/Xeloses)
  * @package    HumanFriendlyConfig (https://github.com/Xeloses/human-friendly-config)
- * @version    1.1
+ * @version    1.0.1
  * @copyright  Xeloses 2019-2020
  * @license    MIT (http://en.wikipedia.org/wiki/MIT_License)
  */
