a7e5c6a1d638860cd46235a6102bb56f54238b99
Xeloses <xeloses@gmail.com>
2021-04-13 15:00:30 +0300

    fix spelling

--- lib/ConfigFile.php
+++ lib/ConfigFile.php
@@ -7,7 +7,7 @@
  * @package    HumanFriendlyConfig (https://github.com/Xeloses/human-friendly-config)
  * @version    1.0.1
  * @copyright  Xeloses 2019-2020
- * @license    MIT (http://en.wikipedia.org/wiki/MIT_License)
+ * @license    GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
  */
 
 namespace Xeloses\HumanFriendlyConfig;
